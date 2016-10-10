8c8e0e73ff64427742f02b6fe46a1cc627dc2d18
Alec Ananian <alec.ananian@warnerbros.com>
2016-10-10 11:49:00 -0700

    Updated to podspec version 2.1.0; updated example project

--- Example/NextGenDataManagerExample/ViewController.swift
+++ Example/NextGenDataManagerExample/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        do {
-            try NGDMManifest.sharedInstance.loadManifestXMLFile(Bundle.main.path(forResource: "mos_hls_manifest_r60-v0.4", ofType: "xml")!)
-            print("Main Experience ID: \(NGDMManifest.sharedInstance.mainExperience?.id)")
-        } catch {
-            print("Error loading manifest file")
+        if let filePath = Bundle.main.path(forResource: "mos_manifest", ofType: "xml") {
+            NGDMManifest.createInstance()
+            
+            do {
+                try NGDMManifest.sharedInstance.loadManifestXMLFile(filePath)
+                print("Main Experience ID: \(NGDMManifest.sharedInstance.mainExperience?.id)")
+            } catch {
+                print("Error loading Manifest file")
+            }
+        } else {
+            print("No Manifest file found")
         }
     }
     
